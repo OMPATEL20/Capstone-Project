1c2402eaab1e1064bea5f51edeec406b3818d6b9
OMPATEL20 <124259661+OMPATEL20@users.noreply.github.com>
2025-03-13 00:09:42 -0600

    Update App.js

--- ai_chatbot_frontend/src/App.js
+++ ai_chatbot_frontend/src/App.js
@@ -5,6 +5,10 @@ import UrbanRegistrationForm from "./components/UrbanRegistrationForm";
 import ChatbotPage from "./components/ChatbotPage";
 import BlogPost from "./components/BlogsPage";
 import AddPost from "./components/AddPost";
+import ForgotPassword from "./components/ForgotPassword";
+import ResetPassword from "./components/ResetPassword";
+import ThankYou from "./components/ThankYou";
+import MFA from "./components/MFA";
 function App() {
   return (
     <>
@@ -14,6 +18,10 @@ function App() {
         <Route path="/main" element={<><Navbar /><ChatbotPage /></>} />
         <Route path="/blogs" element={<><Navbar /><BlogPost /></>} />
          <Route path="/add-post" element={<><Navbar /><AddPost /></>} />
+         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password/:token" element={<ResetPassword />} />
+        <Route path="/thank-you" element={<ThankYou />} />
+        <Route path="/mfa" element={<MFA />} />
       </Routes>
     </>
   );
