53ec346d502188de8e7a68aaf605f25cb06fa62b
OMPATEL20 <124259661+OMPATEL20@users.noreply.github.com>
2025-03-12 19:54:03 -0600

    Update App.js

--- ai_chatbot_frontend/src/App.js
+++ ai_chatbot_frontend/src/App.js
@@ -1,53 +1,21 @@
-
-// // import React from 'react';
-// // import LoginForm from './components/LoginForm'; // Ensure the path is correct
-// // function App() {
-  
-// //   return (
-// //     <div className="App">
-// //       <LoginForm />
-// //     </div>
-// //   );
-// // }
-
-// // export default App;
-
-// // src/App.js
-// import React from 'react';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import LoginForm from './components/LoginForm';
-// import MainPage from './MainPage';
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         {/* Login page as the default route */}
-//         <Route path="/" element={<LoginForm />} />
-        
-//         {/* Main page with nested routes for additional sections */}
-//         <Route path="/main/*" element={<MainPage />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginForm from './components/LoginForm';
-import UrbanRegistrationForm from './components/UrbanRegistrationForm';
-import ChatbotPage from './components/ChatbotPage';
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import LoginForm from "./components/LoginForm";
+import UrbanRegistrationForm from "./components/UrbanRegistrationForm";
+import ChatbotPage from "./components/ChatbotPage";
+import BlogPost from "./components/BlogsPage";
+import AddPost from "./components/AddPost";
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Routes>
         <Route path="/" element={<LoginForm />} />
-        <Route path="/register" element={<UrbanRegistrationForm />} />
-        <Route path="/main" element={<ChatbotPage />} />
+        <Route path="/register" element={<><Navbar /><UrbanRegistrationForm /></>} />
+        <Route path="/main" element={<><Navbar /><ChatbotPage /></>} />
+        <Route path="/blogs" element={<><Navbar /><BlogPost /></>} />
+         <Route path="/add-post" element={<><Navbar /><AddPost /></>} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
