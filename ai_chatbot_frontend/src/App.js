f884121f830fa2e5ea53fb56e25658efb33a656e
OMPATEL20 <124259661+OMPATEL20@users.noreply.github.com>
2025-03-18 18:38:53 -0600

    Update App.js

--- ai_chatbot_frontend/src/App.js
+++ ai_chatbot_frontend/src/App.js
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route, Navigate, useLocation, matchPath } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import LoginForm from "./components/LoginForm";
 import UrbanRegistrationForm from "./components/UrbanRegistrationForm";
@@ -9,19 +10,53 @@ import ForgotPassword from "./components/ForgotPassword";
 import ResetPassword from "./components/ResetPassword";
 import ThankYou from "./components/ThankYou";
 import MFA from "./components/MFA";
+import AdminDashboard from "./components/AdminDashboard";
+import AddMarkdownPage from "./components/AddMarkdownPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import AdminProtectedRoute from "./components/AdminProtectedRoute";
+import EventForm from "./components/EventForm";
 function App() {
+  const location = useLocation();
+
+  const noNavbarPaths = [
+    "/",
+    "/register",
+    "/forgot-password",
+    "/reset-password/:token",
+    "/mfa",
+    "/thank-you",
+  ];
+
+  const showNavbar = !noNavbarPaths.some(path => matchPath(path, location.pathname));
+
   return (
     <>
+      {showNavbar && <Navbar />}
+
       <Routes>
+        {/* Public Routes without Navbar */}
         <Route path="/" element={<LoginForm />} />
-        <Route path="/register" element={<><Navbar /><UrbanRegistrationForm /></>} />
-        <Route path="/main" element={<><Navbar /><ChatbotPage /></>} />
-        <Route path="/blogs" element={<><Navbar /><BlogPost /></>} />
-         <Route path="/add-post" element={<><Navbar /><AddPost /></>} />
-         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/register" element={<UrbanRegistrationForm />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password/:token" element={<ResetPassword />} />
         <Route path="/thank-you" element={<ThankYou />} />
         <Route path="/mfa" element={<MFA />} />
+
+        {/* Protected User Routes with Navbar */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/main" element={<ChatbotPage />} />
+          <Route path="/blogs" element={<BlogPost />} />
+          <Route path="/add-post" element={<AddPost />} />
+          <Route path="/EventForm" element={<EventForm />} />
+          {/* Admin Protected Routes */}
+          <Route element={<AdminProtectedRoute />}>
+            <Route path="/admin-dashboard" element={<AdminDashboard />} />
+            <Route path="/add-markdown-page" element={<AddMarkdownPage />} />
+          </Route>
+        </Route>
+
+        {/* Fallback Route */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </>
   );
